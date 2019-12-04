be6dd09acfa0baccbb499cb811404fb548fa8360
Matthew Masiello <sotek222@yahoo.com>
2019-12-04 11:47:00 -0500

    first solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -9,6 +9,48 @@
 //   reverseInt(-90) === -9
 
 // start with leading 0, gets interpreted as octal by JS.
-function reverseInt(n) {}
+
+// 1. Numbers with trailing 0's, lose the 0's when reversed.
+// 2. Negative numbers remain negative on reversal.
+
+// Solution #1:
+function reverseInt(n) {
+  const arr = n.toString().split('');
+
+  const revArr = arr.reduce((acc, cv) => {
+    acc.unshift(cv);
+    return acc;
+  }, []);
+
+  if (n < 0) revArr.unshift(revArr.pop());
+  return Number(revArr.join(''));
+};
 
 module.exports = reverseInt;
+
+// Solution #1:
+// function reverseInt(n) {
+//   const arr = n.toString().split('');
+//   let negative = n < 0 ? arr.shift() : null;
+
+//   const revArr = arr.reduce((acc, cv) => {
+//     acc.unshift(cv);
+//     return acc;
+//   }, []);
+
+//   if (negative) revArr.unshift(negative);
+//   return Number(revArr.join(''));
+// };
+
+// Solution #1 (slight revision):
+// function reverseInt(n) {
+//   const arr = n.toString().split('');
+
+//   const revArr = arr.reduce((acc, cv) => {
+//     acc.unshift(cv);
+//     return acc;
+//   }, []);
+
+//   if (n < 0) revArr.unshift(revArr.pop());
+//   return Number(revArr.join(''));
+// };
