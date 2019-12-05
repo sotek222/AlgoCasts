a094da3acae80ae92a2dce05d50f8b3ddd1251cc
Matthew Masiello <sotek222@yahoo.com>
2019-12-05 11:15:55 -0500

    first solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// Questions: 
+// 1. Are we counting non alphanumeric characters?
+// 2. Should we alway expect the input to be a string, or can it 
+
+// Solution #1:
+function maxChar(str) {
+  const charMap = new Map();
+
+  for (const char of str) {
+    charMap[char] ? charMap[char] += 1 : charMap[char] = 1;
+  };
+
+  let max = "";
+
+  for (const char in charMap) {
+    if (charMap[char] > charMap[max] || max === "") max = char;
+  };
+
+  return max;
+};  
 
 module.exports = maxChar;
--- exercises/maxchar/test.js
+++ exercises/maxchar/test.js
@@ -12,3 +12,7 @@ test('Finds the most frequently used char', () => {
 test('Works with numbers in the string', () => {
   expect(maxChar('ab1c1d1e1f1g1')).toEqual('1');
 });
+
+test('Works with non-alphanumeric characters and non-digit characters', () => {
+  expect(maxChar('aa!!a?aadvca!!!!trt!!!')).toEqual('!');
+});
