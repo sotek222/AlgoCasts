a7e89d8ef2a5fe421763627b78a0ebd50511f945
Matthew Masiello <sotek222@yahoo.com>
2019-12-05 11:42:19 -0500

    slight refactor

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -7,23 +7,35 @@
 
 // Questions: 
 // 1. Are we counting non alphanumeric characters?
-// 2. Should we alway expect the input to be a string, or can it 
+// 2. Should we alway expect the input to be a string, or should we handle bad inputs?
+// 3. Do we count Uppercase chars as distinct from their lowercase counterpart?
 
-// Solution #1:
+// Solution #2:
 function maxChar(str) {
   const charMap = new Map();
 
   for (const char of str) {
-    charMap[char] ? charMap[char] += 1 : charMap[char] = 1;
+    charMap[char] = charMap[char] + 1 || 1;
   };
 
-  let max = "";
-
-  for (const char in charMap) {
-    if (charMap[char] > charMap[max] || max === "") max = char;
-  };
-
-  return max;
+  return Object.keys(charMap).reduce((a, b) =>  charMap[a] > charMap[b] ? a : b);
 };  
 
 module.exports = maxChar;
+
+// Solution #1:
+// function maxChar(str) {
+//   const charMap = new Map();
+
+//   for (const char of str) {
+//     charMap[char] ? charMap[char] += 1 : charMap[char] = 1;
+//   };
+
+//   let max = "";
+
+//   for (const char in charMap) {
+//     if (charMap[char] > charMap[max] || max === "") max = char;
+//   };
+
+//   return max;
+// };  
