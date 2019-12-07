b35ca01978fbbc2793b3c001db06808f7bff21b1
Matthew Masiello <sotek222@yahoo.com>
2019-12-07 12:55:18 -0500

    third solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,18 +8,19 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+// Solution #3:
 function chunk(array, size) {
-  const chunkArr = [];
-  array.forEach(ele => {
+  return array.reduce((chunkArr, cv) => {
     const last = chunkArr[chunkArr.length - 1];
     if(!last || last.length === size){
-      chunkArr.push([ele]);
+      chunkArr.push([cv]);
     } else {
-      last.push(ele);
+      last.push(cv);
     }
-  });
-  return chunkArr;
+    return chunkArr;
+  }, []);
 };
+
 
 module.exports = chunk;
 
@@ -41,5 +42,25 @@ module.exports = chunk;
 //     }
 //   };
 
+//   return chunkArr;
+// };
+
+// Solution #2:
+// function chunk(array, size) {
+//   // we create an empty array to store out chunks 
+//   const chunkArr = [];
+//   array.forEach(ele => {
+//   // We find the last element in the chunkArr.
+//     const last = chunkArr[chunkArr.length - 1];
+//     //  If the last element does not exist or its length is the size input
+//     if(!last || last.length === size){
+//    //  push a new array literal into the chunk array, with the current ele inside
+//       chunkArr.push([ele]);
+//     } else {
+//       // other wise if we find last or its not at the right size, push the curent
+//       // ele into last.
+//       last.push(ele);
+//     }
+//   });
 //   return chunkArr;
 // };
