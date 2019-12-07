3265711b6bc2960197c4985970d6f0ca58028af0
Matthew Masiello <sotek222@yahoo.com>
2019-12-07 12:03:26 -0500

    first solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,40 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const chunkArr = [];
+  let subArr = [];
+
+  for(let i = 0; i < array.length; i++){
+    subArr.push(array[i]);
+
+    if(subArr.length === size || i === (array.length - 1)){
+      chunkArr.push(subArr);
+      subArr = [];
+    }
+  };
+
+  return chunkArr;
+};
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+//   // This array will store our chunks
+//   const chunkArr = [];
+//   // This binding will store the data of each chunk
+//   let subArr = [];
+
+//   for (let i = 0; i < array.length; i++) {
+//     subArr.push(array[i]);
+
+//     // If the subArr is at the right size or we are at the end of the loop
+//     // push the subArr into the chunkArr
+//     if (subArr.length === size || i === (array.length - 1)) {
+//       chunkArr.push(subArr);
+//       subArr = [];
+//     }
+//   };
+
+//   return chunkArr;
+// };
