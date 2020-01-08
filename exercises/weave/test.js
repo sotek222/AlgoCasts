3fdeceed2bb68b8f2044d9cffbe698b6da8ee33c
Matthew Masiello <sotek222@yahoo.com>
2020-01-08 14:27:21 -0500

    remove console.log

--- exercises/weave/test.js
+++ exercises/weave/test.js
@@ -31,9 +31,7 @@ test('weave can combine two queues', () => {
   two.add('two');
   two.add('three');
   two.add('four');
-  console.log(one, two)
   const result = weave(one, two);
-  console.log(result);
   expect(result.remove()).toEqual(1);
   expect(result.remove()).toEqual('one');
   expect(result.remove()).toEqual(2);
