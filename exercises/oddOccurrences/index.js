f80f7d452ad1dad1762f228ae047c958c03c809b
Matthew Masiello <sotek222@yahoo.com>
2020-02-05 16:51:52 -0500

    first solution

--- exercises/oddOccurrences/index.js
+++ exercises/oddOccurrences/index.js
@@ -1,9 +1,22 @@
 /*
- * Given an array of numbers, only one number is repeated an odd number
- * of times. Return that number. You can assume all numbers are positive.
- * You can also assume only one number occurs an odd number of times.
+Given an array of numbers, only one number is repeated an odd number
+of times. Return that number. You can assume all numbers are positive.
+You can also assume only one number occurs an odd number of times.
  */
 
-function oddOccurrence(arr) {}
+function oddOccurrence(arr) {
+  const occurences = {};
+
+  arr.forEach(number => {
+    occurences[number] = occurences[number] + 1 || 1;
+  });
+
+  for(let number in occurences){
+    if (occurences[number] % 2 !== 0){
+      return Number(number);
+    };
+  };
+
+};
 
 module.exports = oddOccurrence;
