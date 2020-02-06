85fc762b515df000aef13a496f2a8934a0c77e3d
Matthew Masiello <sotek222@yahoo.com>
2020-02-06 16:16:36 -0500

    second solution

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -1,7 +1,7 @@
 // --- Directions
-// Given the root node of a tree, return
-// an array where each element is the width
-// of the tree at each level.
+// 
+
+
 // --- Example
 // Given:
 //     0
@@ -11,6 +11,41 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+// Solution #2:
+function levelWidth(root) {
+  const results = [0];
+  const queue = [root, "stop"];
+
+  while(queue.length > 1){
+    const node = queue.shift();
+    
+    if (node === "stop") {
+      queue.push("stop");
+      results.push(0);
+    } else {
+      results[results.length - 1]++;
+      queue.push(...node.children);
+    };
+  };
+  return results;
+}
 
 module.exports = levelWidth;
+// Solution #1:
+// function levelWidth(root) {
+//   const results = [0];
+//   const queue = [root, "stop"];
+
+//   while (queue.length > 1) {
+//     const node = queue.shift();
+//     queue.push(...node.children);
+//     results[level] = results[level] + 1;
+
+//     if (queue[0] === "stop" && queue.length > 1) {
+//       queue.push(queue.shift());
+//       results.push(0);
+//       level++;
+//     };
+//   };
+//   return results;
+// };
