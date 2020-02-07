016d0d2415995f85e2e842ecfe443161e2913721
Matthew Masiello <sotek222@yahoo.com>
2020-02-07 14:20:04 -0500

    first implementation of insert for BST

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,33 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data, right = null, left = null){
+    this.data = data;
+    this.right = right;
+    this.left = left;
+  }
+
+  insert(data){
+    let node = this;
+    let prevNode = this;
+
+    while(node) {
+      if(data > node.data){
+        prevNode = node;
+        node = node.right;
+      } else {
+        prevNode = node;
+        node = node.left;
+      };
+    };
+    const newNode = new Node(data);
+    newNode.data > prevNode.data ? 
+    prevNode.right = newNode : 
+    prevNode.left = newNode;
+  }
+
+
+}
 
 module.exports = Node;
