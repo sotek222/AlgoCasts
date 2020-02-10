5e67217133b82c0dfd15906edd54dc1c1d8d162b
Matthew Masiello <sotek222@yahoo.com>
2020-02-10 10:35:19 -0500

    second implementation of insert

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -19,22 +19,18 @@ class Node {
   }
 
   insert(data){
-    let node = this;
-    let prevNode = this;
-
-    while(node) {
-      if(data > node.data){
-        prevNode = node;
-        node = node.right;
-      } else {
-        prevNode = node;
-        node = node.left;
-      };
+    if(data < this.data && this.left){
+      this.left.insert(data);
+    } else if (data < this.data){
+      this.left = new Node(data);
     };
-    const newNode = new Node(data);
-    newNode.data > prevNode.data ? 
-    prevNode.right = newNode : 
-    prevNode.left = newNode;
+
+    if (data > this.data && this.right) {
+      this.right.insert(data);
+    } else if (data > this.data) {
+      this.right = new Node(data);
+    };
+
   }
 
   contains(data){
@@ -60,3 +56,51 @@ class Node {
 }
 
 module.exports = Node;
+// First implementation:
+// class Node {
+//   constructor(data) {
+//     this.data = data;
+//     this.right = null;
+//     this.left = null;
+//   }
+
+//   insert(data) {
+//     let node = this;
+//     let prevNode = this;
+
+//     while (node) {
+//       if (data > node.data) {
+//         prevNode = node;
+//         node = node.right;
+//       } else {
+//         prevNode = node;
+//         node = node.left;
+//       };
+//     };
+//     const newNode = new Node(data);
+//     newNode.data > prevNode.data ?
+//       prevNode.right = newNode :
+//       prevNode.left = newNode;
+//   }
+
+//   contains(data) {
+//     let node = this;
+
+//     if (node.data === data) {
+//       return node;
+//     } else {
+//       if (node.left || node.right) {
+//         if (data > node.data) {
+//           node = node.right;
+//           return node.contains(data);
+//         } else {
+//           node = node.left;
+//           return node.contains(data);
+//         }
+//       } else {
+//         return null;
+//       };
+//     };
+//   }
+
+// };
