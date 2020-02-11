054d72203e16db4958b955a10ee3e804558bf918
Matthew Masiello <sotek222@yahoo.com>
2020-02-10 19:31:44 -0500

    second solution

--- exercises/bsz/index.js
+++ exercises/bsz/index.js
@@ -1,5 +1,20 @@
 function bsz(arr){
-
+  return [...arr.filter(n => n !== 0), ...arr.filter(n => n == 0)];
 };
 
 module.exports = bsz;
+
+// Solution #1: 
+// function bsz(arr) {
+//   const zeroes = [];
+//   const nonZeroes = [];
+
+//   arr.forEach((number) => {
+//     if (number === 0) {
+//       zeroes.push(number);
+//     } else {
+//       nonZeroes.push(number);
+//     };
+//   });
+//   return [...nonZeroes, ...zeroes];
+// };
