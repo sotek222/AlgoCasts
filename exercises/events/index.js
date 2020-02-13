5fbd77f42cac7f3fc2facc6a82c6acc0a27b9dda
Matthew Masiello <sotek222@yahoo.com>
2020-02-13 11:05:45 -0500

    first implementation of Event Class

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,35 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor(){
+    this.eventsRegistry = {};
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if(this.eventsRegistry[eventName]) {
+      this.eventsRegistry[eventName].push(callback);
+    } else {
+      this.eventsRegistry[eventName] = [callback];
+    } 
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (this.eventsRegistry[eventName]) {
+      this.eventsRegistry[eventName].forEach(function(cb){
+        cb();
+      })
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    if (this.eventsRegistry[eventName]){
+      this.eventsRegistry[eventName] = [];
+    }
+  }
 }
 
 module.exports = Events;
